e9b24584f58040b8b57a78e953966965e3894a3b
Fabien Chouteau <chouteau@adacore.com>
2020-10-02 12:06:57 +0200

    First import

---
+++ src/adl_config.ads
@@ -0,0 +1,9 @@
+
+--  This file is supposed to be generated from a configuration.
+--  For Alire we use this file with default values.
+
+package ADL_Config is
+   Max_Mount_Points      : constant := 2;
+   Max_Mount_Name_Length : constant := 128;
+   Max_Path_Length       : constant := 1024;
+end ADL_Config;
