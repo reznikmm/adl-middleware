0cce56ab0dff02527275679a1bde8ae0eaf483f3
Fabien Chouteau <chouteau@adacore.com>
2022-11-28 16:13:37 +0100

    Update with recent Alire features

--- src/adl_config.ads
+++ src/adl_config.ads
@@ -2,8 +2,13 @@
 --  This file is supposed to be generated from a configuration.
 --  For Alire we use this file with default values.
 
+with Adl_Middleware_Config;
+
 package ADL_Config is
-   Max_Mount_Points      : constant := 2;
-   Max_Mount_Name_Length : constant := 128;
-   Max_Path_Length       : constant := 1024;
+   Max_Mount_Points : constant := Adl_Middleware_Config.Max_Mount_Points;
+
+   Max_Mount_Name_Length : constant :=
+     Adl_Middleware_Config.Max_Mount_Name_Length;
+
+   Max_Path_Length : constant := Adl_Middleware_Config.Max_Path_Length;
 end ADL_Config;
